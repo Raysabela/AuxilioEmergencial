efc5f6f6e7c94a5aa3261f21b25f20a85cb7e185
rafaelaalvesn <rafaelaalvesn@hotmail.com>
2020-04-07 18:57:29 -0300

    Inclusão do o enunciado

--- AuxilioEmergencial/AuxilioEmergencial/Program.cs
+++ AuxilioEmergencial/AuxilioEmergencial/Program.cs
@@ -8,6 +8,32 @@ namespace AuxilioEmergencial
 {
     class Program
     {
+
+        /*
+         
+             **  Criar um programa que calcule a pessoa tem direito ou não ao auxílio emergencial. Fazer as perguntas, com base nas respostas, retornar se tem direito ou não.
+
+
+Regras:
+
+- Ser maior de 18 anos de idade
+- Não ter emprego formal
+- Não receber benefício previdenciário ou assistencial, seguro-desemprego ou de outro programa de transferência de renda federal que não seja o Bolsa Família;
+- Renda familiar mensal per capita (por pessoa) de até meio salário mínimo (R$ 522,50) ou renda familiar mensal total (tudo o que a família recebe) de até três salários mínimos (R$ 3.135,00); 
+- Não ter recebido rendimentos tributáveis, no ano de 2018, acima de R$ 28.559,70;
+- Exercer atividade na condição de microempreendedor individual (MEI) ou ser contribuinte individual ou facultativo do Regime Geral de Previdência Social (RGPS) ou ser trabalhador informal inscrito no Cadastro Único para Programas Sociais do Governo Federal (CadÚnico);
+
+
+
+
+* O programa deve ter os seguintes funções obrigatoriamente:
+
+- Método para calcular a renda-por capita, recebendo como parâmetro o número de pessoas na família e a renda total da família
+- Método booleano que retorna se a pessoa é maior de idade ou não, recebendo a idade como parâmetro 
+
+* O programa deve cobrir ao máximos os erros que o usuário pode cometer.
+             */
+
         static void Main(string[] args)
         {
         }
